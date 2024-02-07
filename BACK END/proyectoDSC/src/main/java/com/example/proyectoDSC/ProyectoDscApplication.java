52c2caa83a08bbfdd19088bcbd05a2246fb0e325
Marlon <marlonyuyi@gmail.com>
2024-02-06 22:31:44 -0400

    se sube carpeta back a la rama DEV

---
+++ BACK END/proyectoDSC/src/main/java/com/example/proyectoDSC/ProyectoDscApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoDSC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDscApplication.class, args);
+	}
+
+}
---
+++ BACK END/proyectoDSC/src/test/java/com/example/proyectoDSC/ProyectoDscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoDSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
